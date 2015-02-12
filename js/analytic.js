67a58cce09370e3d9debfb6762dddb536ed9aa86
sahel.mastoureshgh@cbsinteractive.com <sahel.mastoureshgh@cbsinteractive.com>
2015-02-12 10:06:05 -0500

    Added js file

---
+++ js/analytic.js
@@ -0,0 +1,5 @@
+(function(w,g){w['GoogleAnalyticsObject']=g;
+w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+//replaced with my Google Analytics profile ID.
+ga('create', 'UA-59663837-1','auto');
+ga('send', 'pageview');
